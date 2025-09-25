fbe6bcbf9c70ee1867608feeb6144c13b79ff185
Mateo Morales <Moralmat76@gmail.com>
2025-09-25 12:18:07 -0500

    Variable App 1

---
+++ src/main/java/VariableApp.java
@@ -0,0 +1,10 @@
+public class VariableApp{
+    public static void main(String[] args)  {
+        String color = "Green";
+        Integer Year_Start = 2025;
+        String middle_initial = "G";
+        boolean Do_You_Have_Pets = true;
+        String Quote = "You'll get there";
+
+    }
+}
