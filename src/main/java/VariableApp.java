77eacebd8f737cac412f25d336bcd362d2ebc2e2
Mateo Morales <Moralmat76@gmail.com>
2025-09-25 12:30:28 -0500

    Variable App 1

--- src/main/java/VariableApp.java
+++ src/main/java/VariableApp.java
@@ -5,6 +5,10 @@ public class VariableApp{
         String middle_initial = "G";
         boolean Do_You_Have_Pets = true;
         String Quote = "You'll get there";
-
+        System.out.println("The color is: " + color);
+        System.out.println("The starting year is: " + Year_Start);
+        System.out.println("The middle initial is: " + middle_initial);
+        System.out.println("Do you have pets? " + Do_You_Have_Pets);
+        System.out.println("The quote is: " + Quote);
     }
 }
