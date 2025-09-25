58fcd73c2a5f54555cc7fa0f67f9aa4455c86a4d
Mateo Morales <Moralmat76@gmail.com>
2025-09-25 13:01:03 -0500

    Variable App 2

--- src/main/java/VariableApp.java
+++ src/main/java/VariableApp.java
@@ -10,5 +10,13 @@ public class VariableApp{
         System.out.println("The middle initial is: " + middle_initial);
         System.out.println("Do you have pets? " + Do_You_Have_Pets);
         System.out.println("The quote is: " + Quote);
+        int days_in_week = 7;
+        double coffee_price = 4.99;
+        char favorite_letter = 'M';
+        boolean is_raining = false;
+        System.out.println("There are " + days_in_week + " days in a week.");
+        System.out.println("The price of coffee is $" + coffee_price + ".");
+        System.out.println("My favorite letter is " + favorite_letter + ".");
+        System.out.println("Is it raining outside? " + is_raining + ".");
     }
 }
